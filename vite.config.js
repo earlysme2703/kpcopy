edda725870f29989d059c1c1ca6b67563a778966
M. Early Maulana <142871177+earlysme2703@users.noreply.github.com>
2025-04-10 01:41:39 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: ['resources/views/**/*.blade.php'],
+            refresh: true,
         }),
     ],
 });
